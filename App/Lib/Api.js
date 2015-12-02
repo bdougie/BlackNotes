2ef56b9fbae767a474db819e84aca95b496c5312
Brian Douglas <ilikerobot@gmail.com>
2015-12-02 05:40:42 -0800

    adds delete api

--- App/Lib/Api.js
+++ App/Lib/Api.js
@@ -1,17 +1,24 @@
 let api = {
   getNotes() {
-    let url = `https://blacknotes.firebaseio.com/.json`;
+    let url = `https://blacknotes.firebaseio.com/testUser.json`;
     return fetch(url).then((res) => res.json())
   },
   addNote(text) {
-    let url = `https://blacknotes.firebaseio.com/.json`;
+    let url = `https://blacknotes.firebaseio.com/testUser.json`;
 
     return fetch(url, {
       method: 'post',
       body: JSON.stringify(text)
     }).then((res) => res.json());
-  }
+  },
+  deleteNote(text) {
+    let url = `https://blacknotes.firebaseio.com/testUser.json`;
 
+    return fetch(url, {
+      method: 'delete',
+      body: JSON.stringify(text)
+    });
+  }
 };
 
 export default api;
